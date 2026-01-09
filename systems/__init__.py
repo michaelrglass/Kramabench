8c4fe658be4caa66ac4f0943d6560e206cc84a26
Michael Glass <mrglass@us.ibm.com>
2026-01-09 14:02:23 -0500

    OpenHands SUT

--- systems/__init__.py
+++ systems/__init__.py
@@ -4,3 +4,4 @@ This module provides system-level functionality and configurations.
 """
 from .baseline_example import ExampleBaselineSystem
 from .dsguru import *
+from .openhands.openhands_system import OpenHands
